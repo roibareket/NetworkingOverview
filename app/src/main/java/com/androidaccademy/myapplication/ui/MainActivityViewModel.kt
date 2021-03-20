baeba870fac4ef0a816e4f486e58f1ef3402b1e8
roibareket <roi.bareket@tg-17.com>
2021-03-20 16:41:35 +0200

    .

--- app/src/main/java/com/androidaccademy/myapplication/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/androidaccademy/myapplication/ui/MainActivityViewModel.kt
@@ -1,17 +1,19 @@
 package com.androidaccademy.myapplication.ui
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
 import com.androidaccademy.myapplication.network.NetworkManager
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.Observables
+import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 
 class MainActivityViewModel : ViewModel() {
 	private val networkManager = NetworkManager()
 
+	val compositeDisposable = CompositeDisposable()
 	val networkCallsProgressLiveData = MutableLiveData(NetworkCallProgress.IDLE)
 	val networkCallsCompletedLiveEvent = networkCallsProgressLiveData
 		.map { Event(it) }
@@ -30,27 +32,14 @@ class MainActivityViewModel : ViewModel() {
 			}, {
 				Timber.i(it, "network failed")
 			})
+			.addTo(compositeDisposable)
 	}
 
-	fun getDataFromMultipleReposInitial() {
-		networkManager.gitHubService.listRepos("roibareket")
-			.subscribeOn(Schedulers.io())
-			.subscribe({
-				Timber.i(it.toString())
-			}, {
-				Timber.i(it, "network failed")
-			})
-
-		networkManager.gitHubService.listRepos("parahall")
-			.subscribeOn(Schedulers.io())
-			.subscribe({
-				Timber.i(it.toString())
-			}, {
-				Timber.i(it, "network failed")
-			})
+	override fun onCleared() {
+		super.onCleared()
+		compositeDisposable.clear()
 	}
 }
-
 
 enum class NetworkCallProgress {
 	IDLE, EXECUTING, COMPLETED
